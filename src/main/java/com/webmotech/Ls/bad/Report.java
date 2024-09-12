7bc22b68e1a2fd4e1bee436626c644cd99de2bbb
Irosh Perera <123479718+IroshPerera@users.noreply.github.com>
2024-09-12 15:23:26 +0530

    Delete src/main/java/com/webmotech/Ls directory

--- src/main/java/com/webmotech/Ls/bad/Report.java
+++
@@ -1,8 +0,0 @@
-package com.webmotech.Ls.bad;
-
-public abstract class Report {
-    public abstract void generate();
-    public void save() {
-        System.out.println("Saving report.");
-    }
-}
