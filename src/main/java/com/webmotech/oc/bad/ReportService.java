ad191677415aa0ef88731b6060ddf05b8b79e41a
Irosh Perera <irosh@webmotech.com>
2024-09-12 14:50:22 +0530

    open/closed principle - bad

---
+++ src/main/java/com/webmotech/oc/bad/ReportService.java
@@ -0,0 +1,18 @@
+package com.webmotech.oc.bad;
+
+public class ReportService {
+    public void generateReport(String type) {
+        if (type.equals("Monthly")) {
+            System.out.println("Generating monthly report");
+        } else if (type.equals("Yearly")) {
+            System.out.println("Generating yearly report");
+        } else {
+            System.out.println("Invalid report type");
+        }
+    }
+
+    public void formatReport(String type) {
+        generateReport(type);
+        System.out.println("Formatting report");
+    }
+}
