ea3e95d903fb89aed67c6982824655f66202f113
Irosh Perera <irosh@webmotech.com>
2024-09-12 15:25:44 +0530

    liskov substitution principle - good

---
+++ src/main/java/com/webmotech/Ls/good/MonthlyReport.java
@@ -0,0 +1,8 @@
+package com.webmotech.Ls.good;
+
+public class MonthlyReport extends Report {
+    @Override
+    public void generate() {
+        System.out.println("Generating monthly sales report.");
+    }
+}
---
+++ src/main/java/com/webmotech/Ls/good/YearlyReport.java
@@ -0,0 +1,8 @@
+package com.webmotech.Ls.good;
+
+public class YearlyReport extends Report {
+    @Override
+    public void generate() {
+        System.out.println("Generating yearly performance review.");
+    }
+}
